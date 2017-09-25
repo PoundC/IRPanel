a64fd10f30d8c267258086dd8dc1f572edda7879
Jeffrey Roberts <jeffrey.l.roberts@gmail.com>
2017-09-25 09:41:02 -0400

    Shotgun Commit

---
+++ plugins/AdminLTE/src/View/Helper/DashboardHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AdminLTE\View\Helper;
+
+use Cake\View\Helper as Helper;
+use Cake\View\View;
+
+class DashboardHelper extends Helper
+{
+    public function FullWidget($count, $title, $link, $color, $icon)
+    {
+        return '<div class="col-lg-3 col-xs-6"><div class="small-box ' . $color . '"><div class="inner"><h3>' . $count . '</h3><p>' . $title . '</p></div><div class="icon"><i class="fa ' . $icon . '"></i></div><a href="' . $link . '" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a></div></div>';
+    }
+}
