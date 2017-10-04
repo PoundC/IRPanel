b6ea5f5c6c204595204bd35bede9c59cc1666875
Jeffrey Roberts <jeffrey.l.roberts@gmail.com>
2017-10-04 13:20:37 -0400

    Stats minor fix

---
+++ config/Migrations/20171004171926_AddColumnToStats.php
@@ -0,0 +1,28 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddColumnToStats extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('stats_configs');
+        $table->addColumn('orderby', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('timestamp', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
