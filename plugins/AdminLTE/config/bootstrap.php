309d5cee3009a7c7b87c4c25f0ac6eb797fb61fe
Jeffrey Roberts <Jeffrey.L.Roberts@gmail.com>
2019-10-26 13:19:04 -0400

    Moving around permissions to plugins

--- plugins/AdminLTE/config/bootstrap.php
+++ plugins/AdminLTE/config/bootstrap.php
@@ -3,5 +3,32 @@
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
 
+$permissions = Configure::read('MyPermissions');
+$someMorePermissions = [
+    [
+        'role' => ['user'],
+        'plugin' => 'AdminLTE',
+        'controller' => ['Messaging'],
+        'action' => ['messageDelete', 'index', 'view', 'compose', 'toAutocomplete', 'sendReply'],
+        'allowed' => true,
+    ],
+    [
+        'role' => ['user'],
+        'plugin' => 'AdminLTE',
+        'controller' => ['Notifications'],
+        'action' => ['index', 'view', 'growl'],
+        'allowed' => true,
+    ],
+    [
+        'role' => ['user'],
+        'plugin' => 'AdminLTE',
+        'controller' => ['Tasks'],
+        'action' => ['index', 'delete'],
+        'allowed' => true
+    ]
+];
+$permissions = array_merge((array)$permissions, $someMorePermissions);
+Configure::write('MyPermissions', $permissions);
+
 Configure::write('Users.config', ['users']);
 Plugin::load('CakeDC/Users', ['routes' => false, 'bootstrap' => true]);
