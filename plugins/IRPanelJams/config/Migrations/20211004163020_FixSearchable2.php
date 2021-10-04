abe2111c4a8bcd5309479756a3891d49806c973a
Jeffrey Roberts <Jeffrey.L.Roberts@gmail.com>
2021-10-04 12:32:33 -0400

    Fixing Searchable, Again Again

---
+++ plugins/IRPanelJams/config/Migrations/20211004163020_FixSearchable2.php
@@ -0,0 +1,21 @@
+<?php
+use Migrations\AbstractMigration;
+
+class FixSearchable2 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('i_r_c_jams');
+        $table->changeColumn('searchable', 'text',[
+            'limit' => \Phinx\Db\Adapter\MysqlAdapter::TEXT_MEDIUM
+        ]);
+        $table->save();
+    }
+}
