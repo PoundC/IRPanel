567a6f24ad130c3daa2179d00dabc549928a3893
Jeffrey Roberts <Jeffrey.L.Roberts@gmail.com>
2021-10-06 15:32:09 -0400

    Adding Useless Avatar Field

---
+++ plugins/AdminLTE/config/Migrations/20211006125506_AddAvatarsToUsers.php
@@ -0,0 +1,21 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddAvatarsToUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('avatar', 'string', [
+            'limit' => '512'
+        ]);
+        $table->update();
+    }
+}
